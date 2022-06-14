13cbd76a460a4631c14b369744a7fa537fe28f78
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-14 13:19:24 -0600

    Add user schema and model

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true })); 
+const mongoose = require('mongoose');
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const mySecret = process.env['MONGO_URI'];
 require('dotenv').config()
+
+const userSchema = new Schema({
+  username: { type: String, required: true }
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
