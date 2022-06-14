c6110f8e74bee6a995d7aa80c8ff0c9660d270c8
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-14 16:41:42 -0600

    Add endpoints to track exercises and logs for users

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ const Schema = mongoose.Schema;
 const cors = require('cors')
 
 const userSchema = new Schema({
-  username: { type: String, required: true }
+  username: { type: String, required: true },
 });
-
 const User = mongoose.model("User", userSchema);
+
+const findUserWithExercises = function() {
+
+}
+
+const exerciseSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now() }
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -38,6 +49,54 @@ app.post('/api/users', (req, res) => {
   User.findOneAndUpdate(filter, filter, { upsert: true, new: true }, (err, matchedUser) => {
     if(err) return console.log(err);
     res.json({ username: matchedUser.username, id: matchedUser._id });
+  });
+});
+
+app.post('/api/users/:id/exercises', (req, res) => {
+  const userId = req.params.id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = req.body.date;
+  const newExercise = {
+    userId: userId,
+    description: description,
+    duration: duration,
+    date: date
+  }
+  Exercise.create(newExercise, (err, createdExercise) => {
+    if(err) return console.log(err);
+  });
+
+  User.findById(userId, (err, matchedUser) => {
+    if(err) return console.log(err);
+    const exerciseQuery = { userId: userId };
+    Exercise.find(exerciseQuery, (exerciseErr, matchedExercises) => {
+      if(exerciseErr) return console.log(exerciseErr);
+      const parsedExercises = matchedExercises.map(exercise => ({
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString(),
+      }));
+      res.json({ ...matchedUser, exercises: parsedExercises });
+    })
+  });
+});
+
+app.get('/api/users/:id/logs', (req, res) => {
+  const userId = req.params.id;
+
+  User.findById(userId, (err, matchedUser) => {
+    if(err) return console.log(err);
+    const exerciseQuery = { userId: userId };
+    Exercise.find(exerciseQuery, (exerciseErr, matchedExercises) => {
+      if(exerciseErr) return console.log(exerciseErr);
+      const parsedExercises = matchedExercises.map(exercise => ({
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString(),
+      }));
+      res.json({ ...matchedUser, exercises: parsedExercises });
+    })
   });
 });
 
